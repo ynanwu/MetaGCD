10bb983b3a0f48d5c77a84a6f529a3b8099e8915
ynanwu <131207836+ynanwu@users.noreply.github.com>
2023-09-23 14:56:34 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+cifar_10_root = '/media/data-vision-server/yananwu/dataset/CIFAR/'
+cifar_100_root = '/media/data-vision-server/yananwu/dataset/CIFAR/'
+imagenet_root = '/scratch/shared/beegfs/shared-datasets/ImageNet/ILSVRC12'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+dino_pretrain_path = '/home/yananwu/myCode/MetaGCD/pretrained_models/dino_vitbase16_pretrain.pth'
+feature_extract_dir = '/MetaGCD/extracted_features_public_impl'     # Extract features to this directory
+exp_root = '/home/yananwu/myCode/MetaGCD/checkpoints/'          # All logs and checkpoints will be saved here
